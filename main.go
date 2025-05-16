eff46071f6643fcdc1a476b4050d38594fc2fe18
Mehmetcan Ece <mehmetcanece02@hotmail.com>
2025-05-17 02:13:58 +0300

    routes ve main.go yazildi, database postgre baglantisina geciliyor.

--- main.go
+++ main.go
@@ -1,19 +1,10 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"simple-payment-api/routes"
 )
 
-
-func main(){
-	r := gin.Default() //default burada otomatik logger ve recovery middleware ile gelen bir router objesi
-
-	// "/" endpoint'ine GET isteği gelirse bu çalışacak
-	r.GET("/", func(c *gin.Context){
-		c.JSON(200, gin.H{
-			"message": "Backend API is running.",
-		})
-	})
-
-	r.Run(":8000")  // 8080 portundan server'ı başlat
+func main() {
+	r := routes.InitRoutes()
+	r.Run(":8000")
 }
---
+++ routes/routes.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"simple-payment-api/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+ 
+func InitRoutes() *gin.Engine{ // Geriye bir *gin.Engine döndürüyor !! Bu, Gin’in HTTP server’ını temsil eder.
+	r := gin.Default() // default logger ve error recovery middleware
+
+	r.GET("/", func(c *gin.Context) { //get endpointi
+		c.JSON(200, gin.H{
+			"message": "Backend API is running"})
+	})
+
+	r.POST("/payments",controllers.PostPayment) //post isteklerini paymentsa yönlendiriyoruz
+
+	return r
+}
